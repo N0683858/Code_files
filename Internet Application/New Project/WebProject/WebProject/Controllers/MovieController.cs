470344b07484bf62836c9cc01e97a1e12d9a3e93
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2018-05-24 17:54:15 +0100

    new commit

---
+++ Internet Application/New Project/WebProject/WebProject/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Recommended()
+        {
+            var movie = new Movie() { Name = "Sharek!" };
+            return View(movie);
+        }
+    }
+}
