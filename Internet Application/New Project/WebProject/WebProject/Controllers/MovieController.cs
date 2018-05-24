edb249b0ecb824d55db0150d14ec3f75b9a66c63
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2018-05-24 18:38:45 +0100

    back to old webproject

--- Internet Application/New Project/WebProject/WebProject/Controllers/MovieController.cs
+++ Internet Application/New Project/WebProject/WebProject/Controllers/MovieController.cs
@@ -15,5 +15,21 @@ namespace WebProject.Controllers
             var movie = new Movie() { Name = "Sharek!" };
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
