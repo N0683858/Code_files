cfd2c4244d5741d9eedbc5feaa3b3d44f2cee954
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2018-05-26 18:53:01 +0100

    fixed the broken database (i think)

--- Internet Application/Bon Appetit/Site.master.cs
+++ Internet Application/Bon Appetit/Site.master.cs
@@ -12,34 +12,5 @@ public partial class Site : System.Web.UI.MasterPage
         set { lblH1.Text = value; }
     }
 
-   /* protected void Page_Load(object sender, EventArgs e)
-    {
-        //fill 'Days Until' label if it hasn't been overridden
-        if (lblDaysUntilHalloween != null)
-        {
-            int daysUntil = DaysUntilHalloween();
-            switch (daysUntil)
-            {
-                case 0:
-                    lblDaysUntilHalloween.Text = "Happy Halloween!";
-                    break;
-                case 1:
-                    lblDaysUntilHalloween.Text = "Tomorrow is Halloween!";
-                    break;
-                default:
-                    lblDaysUntilHalloween.Text = string.Format(
-                        "There are only {0} days left until Halloween!", daysUntil);
-                    break;
-            }
-        }
-    }
-
-    private int DaysUntilHalloween()
-    {
-        DateTime halloween = new DateTime(DateTime.Today.Year, 10, 31);
-        if (DateTime.Today > halloween)
-            halloween = halloween.AddYears(1);
-        TimeSpan ts = halloween - DateTime.Today;
-        return ts.Days;
-    }*/
+   
 }
