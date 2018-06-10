830176fc31020502eeaf634886136685c24a152f
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2018-06-10 01:29:21 +0100

    nothing big

--- Internet Application/Web Project 1/Website/js/products.js
+++ Internet Application/Web Project 1/Website/js/products.js
@@ -3,7 +3,6 @@ function loadXMLDoc() {
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       myFunction(this);
-      myFunction2();
     }
   };
 /* xmlhttp.open("GET", "http://www.w3schools.com/xml/cd_catalog.xml", true); */
@@ -13,7 +12,7 @@ function loadXMLDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table="<tr><th>Artist</th><th>Title</th><th>Price</th><<th>Img</th></tr>";
+  var table="<tr><th>Image</th><th>Name</th><th>Type</th><th>Price</th><th>Order</th></tr>";
   var x = xmlDoc.getElementsByTagName("PRODUCT");
   for (i = 0; i <x.length; i++) {
     table += "<tr><td>" + "<img style='width:60%;' src='" +
@@ -24,14 +23,14 @@ function myFunction(xml) {
     x[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue +
     "</td><td>" +
     x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue +
-    "</td><td>" + "<button type='button'></button>" +
+    "</td><td>" + "<button id='add_btn' type='button'>Add</button>" +
     "</td></tr>";;
   }
   document.getElementById("demo").innerHTML = table;
 }
 
-function myFunction2(){
-  var table = document.getElementById("demo"), sumVal = 0;
+myFunction2(var value){
+/*  var table = document.getElementById("demo"), sumVal = 0;
 
   for(var i = 1; i < table.rows.length; i++)
   {
@@ -39,5 +38,11 @@ function myFunction2(){
   }
 
   document.getElementById("sum").innerHTML = "sum value = " + sumVal;
-  console.log(sumVal);
+  console.log(sumVal); */
+
+  var sum = 0;
+
+  sum += value;
+
+  window.write(sum);
 }
