31d16d6602264ed1f78ec2d296d464cb37c9728f
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2018-06-10 11:23:00 +0100

    was tring to do img slider

--- Internet Application/Web Project 1/Website/js/products.js
+++ Internet Application/Web Project 1/Website/js/products.js
@@ -5,14 +5,14 @@ function loadXMLDoc() {
       myFunction(this);
     }
   };
-/* xmlhttp.open("GET", "http://www.w3schools.com/xml/cd_catalog.xml", true); */
+
   xmlhttp.open("GET", "../html/XML/products.xml", true);
   xmlhttp.send();
 }
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table="<tr><th>Image</th><th>Name</th><th>Type</th><th>Price</th><th>Order</th></tr>";
+  var table="<tr id='tableHeader'><th>Image</th><th>Name</th><th>Type</th><th>Price</th><th>Order</th></tr>";
   var x = xmlDoc.getElementsByTagName("PRODUCT");
   for (i = 0; i <x.length; i++) {
     table += "<tr><td>" + "<img style='width:60%;' src='" +
@@ -21,28 +21,115 @@ function myFunction(xml) {
     x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue +
     "</td><td>" +
     x[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue +
-    "</td><td>" +
+    "</td><td>" + "£" +
     x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue +
-    "</td><td>" + "<button id='add_btn' type='button'>Add</button>" +
+    "</td><td>" + "<button id='add_btn' onclick='myFunction("  + x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue + ");' type='button'>Add</button>" +
     "</td></tr>";;
   }
+  //var tot = "<input name='tot' id='tot' type='number' value='' disabled>"
   document.getElementById("demo").innerHTML = table;
+//  document.getElementById("demo2").innerHTML = tot;
 }
 
-myFunction2(var value){
-/*  var table = document.getElementById("demo"), sumVal = 0;
 
-  for(var i = 1; i < table.rows.length; i++)
+
+/* -------------------------------------------------------------------------------------- */
+/*               Display 1 product at a time with next and prev buttons                   */
+/* -------------------------------------------------------------------------------------- */
+/*
+if(window.XMLHttpRequest)
+{
+  xmlhttp = new XMLHttpRequest();
+}
+
+else {
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xmlhttp.open("GET", "../html/XML/products.xml", true);
+xmlhttp.send();
+xmlDoc = xmlhttp.responseXML;
+
+x = xmlDoc.getElementsByTagName("PRODUCT");
+i = 0;
+
+function display()
+{
+  image = ("<img style='width:60%;' src='" +
+  x[i].getElementsByTagName("imageurl")[0].childNodes[0].nodeValue + "'/>");
+  name = (x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue);
+  type = (x[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue);
+  price = (x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+  button = ("<button id='add_btn' onclick='myFunction("  + x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue + ");' type='button'>Add</button>" )
+
+  txt = "Image: " + image + "<br />Name: " + name + "<br />Type: " type + "<br />Price: " + button;
+  document.getElementById("demo").innerHTML = txt;
+
+}
+
+function next()
+{
+  if(i < x.length - 1)
   {
-    sumVal + parseInt(table.row[i].cells[2].innerHTML);
+    i++;
+    display();
   }
+}
 
-  document.getElementById("sum").innerHTML = "sum value = " + sumVal;
-  console.log(sumVal); */
+function prev()
+{
+  if(i > 0)
+  {
+    i--;
+    display();
+  }
+}
+*/
 
-  var sum = 0;
+/* -------------------------------------------------------------------------------------- */
+/*                                     Captcha                                            */
+/* -------------------------------------------------------------------------------------- */
 
-  sum += value;
+var myInput = document.getElementById("serial"),
+    serialNumber,
+    i,
+    number,
+    verifOne,
+    serial;
 
-  window.write(sum);
+function generate() {
+    "use strict";
+
+    serial = '';
+    serialNumber = "0123456789ABCDEFJHIJKLMNPQRSTUVWXYZabcdefjhijklmnpqrstuvwxyz";
+
+    for (i = 0; i < 4; i = i + 1) {
+        number = Math.floor(Math.random() * 60);
+        serial = serial + serialNumber.substring(number, number + 1);
+    }
+
+    myInput.innerHTML = serial;
+}
+
+function verifyCaptcha() {
+    "use strict";
+
+    verifOne = document.getElementById("entre").value;
+
+    if (verifOne === serial) {
+        alert("Varified!");
+    } else {
+        alert("Could not be varified, try again!");
+    }
+
+}
+
+/* -------------------------------------------------------------------------------------- */
+/*                                     Call the functions                                 */
+/* -------------------------------------------------------------------------------------- */
+function start()
+{
+  loadXMLDoc();
+  //display();
+  generate();
 }
---
+++ Internet Application/Web Project 1/Website/js/slideShow.js
@@ -0,0 +1,53 @@
+
+/* -------------------------------------------------------------------------------------- */
+/*               Previrw images with thumbnail                                            */
+/* -------------------------------------------------------------------------------------- */
+//followed YT tutorial: How to make Image Slider with with thumnail preview (JS) 
+
+/*var lastImg = 1; //Set initial thumbnail and preview
+document.getElementById(0).src = document.getElementById(lastImg).src;
+document.getElementById(lastImg).className = "thumb selected";
+
+function preview(img) {
+    document.getElementById(lastImg).className = "thumb normal";
+    img.className = "thumb selected";
+    document.getElementById(0).src = img.src;
+    lastImg = img.id
+}
+*/
+
+var index=0;
+var titles=[1,2,3];
+
+function moveToNextSlide()
+{
+if (index >= titles.length - 1)
+{
+index=-1;
+}
+var img = document.getElementById("img1");
+index = index + 1;
+var slideName="images/img" + titles[index] + ".jpg";
+img.src=slideName;
+}
+
+function moveToPrevSlide()
+{
+if (index >= titles.length)
+{
+index=+1;
+}
+var img = document.getElementById("img1");
+index = index - 1;
+var slideName="images/img" + titles[index] + ".jpg";
+img.src=slideName;
+}
+
+function random()
+{
+
+  var img = document.getElementById("img1");
+  index = Math.floor((Math.random() * 3) + 0);
+  var slideName="images/img" + titles[index] + ".jpg";
+  img.src=slideName;
+}
